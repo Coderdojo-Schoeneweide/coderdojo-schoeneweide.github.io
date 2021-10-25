54901e18efccfe890b757fd9a48c890c48fb9cc6
Kaes3kuch3n <Kaes3kuch3n@users.noreply.github.com>
2021-10-25 21:03:01 +0000

    deploy: Coderdojo-Schoeneweide/website-hugo@c05979cd89e23090e62f60aefeadc9ba9bb3df0c

--- js/color-theme-switch.js
+++ js/color-theme-switch.js
@@ -1,25 +1,24 @@
+const THEME_KEY = "theme";
+const THEME_LIGHT = "light";
+const THEME_DARK = "dark";
+
 // function to set a given theme/color-scheme
 function setTheme(themeName) {
-  localStorage.setItem("theme", themeName);
+  localStorage.setItem(THEME_KEY, themeName);
   document.documentElement.className = themeName;
 }
 
-// function to toggle between light and dark theme
-function toggleTheme() {
-  if (localStorage.getItem("theme") === "dark") {
-    setTheme("light");
-  } else {
-    setTheme("dark");
-  }
-}
-
 // Immediately invoked function to set the theme on initial load
-(function () {
-  if (localStorage.getItem("theme") === "dark") {
-    setTheme("dark");
-    document.getElementById("slider").checked = false;
-  } else {
-    setTheme("light");
-    document.getElementById("slider").checked = true;
-  }
-})();
+document.addEventListener("DOMContentLoaded", () => {
+  const slider = document.getElementById("slider");
+
+  // Add theme toggle event listener
+  slider.addEventListener("change", (event) =>
+      setTheme(event.target.checked ? THEME_DARK : THEME_LIGHT)
+  );
+
+  // Load theme from local storage
+  const theme = localStorage.getItem(THEME_KEY);
+  setTheme(theme);
+  slider.checked = theme === THEME_DARK;
+});
